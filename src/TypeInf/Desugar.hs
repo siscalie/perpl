167dd44021da43393870e9112567893c36f29666
Sophia Lama <sophialama@macbookpro.dhcp.nd.edu>
2025-12-05 10:43:26 -0500

    Houston I have so many problems </3

--- src/TypeInf/Desugar.hs
+++ src/TypeInf/Desugar.hs
@@ -1,20 +1,22 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Redundant fmap" #-}
+{-# HLINT ignore "Use lambda-case" #-}
 module TypeInf.Desugar where
 
 import qualified Data.Map as Map
 import qualified Data.Set as Set
-import Control.Monad (zipWithM_)
-import Control.Monad.Except (MonadError(throwError), runExcept)
-import Control.Monad.RWS.Lazy (RWST(runRWST))
+import Control.Monad
+import Control.Monad.Except
+import Control.Monad.RWS.Lazy
 import Data.List (intercalate)
 import Scope.Subst (Subst(tmVars, tpVars, tags), STerm(Replace), FreeVars(freeTmVars, freeTpVars), subst, freeVars, freeDatatypes, substTags)
 import Scope.Free (robust, positive, isAff)
 import Scope.Fresh (newVar)
-import Scope.Ctxt (Ctxt, emptyCtxt, ctxtLookupType2, ctxtAddData)
-import Struct.Exprs (SProgs, UsProgs, UsProgs(UsProgs), Case(..), CaseUs(..), Ctor(..), Term(TmCase), Type(TpData), UsTm(UsCase))
-import Struct.Helpers (sortCases, typeof)
-import TypeInf.Check (anyDupDefs, constrain, constrainIf, freshTag, freshTp, guardM, inEnvs, infer, localCurExpr, lookupCtorType,
-    CheckM, CheckR(CheckR), Constraint(Robust, Unify), Loc(Loc), TypeError(WrongNumArgs, WrongNumCases, MissingCases) )
-import TypeInf.Solve ()
+import Scope.Ctxt
+import Struct.Exprs
+import Struct.Helpers
+import TypeInf.Check
+import TypeInf.Solve
 import Util.Graph (scc, SCC(..))
 import Util.Helpers (listDifference, pickyZip)
 
@@ -42,39 +44,33 @@ also RECALL these definitions:
 -}
 
 -- Infers/checks a term, elaborating it from a user-term (UsTm) to a full Term
-desugar' :: UsTm -> CheckM UsTm
+desugar' :: UsTm -> UsTm
 desugar' (UsCase tm cs) =
   -- (CALCULATES the missing cases by running lookupCtorType to get the constructors this type is supposed to have)
   -- (EX: can deduce that a List is either a Nil or a Cons)
-  -- *** Can we use lookupCtorType in our mini-phase? ***
   lookupCtorType cs >>= \ (y, tgs, ps, ctors) -> -- lookup the datatype we have cases for
-  --mapM (const freshTag) tgs >>= \ itgs -> -- pick fresh tags
-  --mapM (const freshTp) ps >>= \ ips -> -- and pick fresh type vars
   let missingCases = listDifference [y | (Ctor y _) <- ctors] [x | (CaseUs x _ _) <- cs] in -- here we're saying that missing cases = ctors - cases
   guardM (null missingCases) (MissingCases missingCases) >> -- guard against missing cases
   guardM (length ctors == length cs) (WrongNumCases (length ctors) (length cs)) >> -- guard against wrong # of cases
-  --infer tm >>= \ tm' ->
-  -- add a constraint here v that (y itgs ips) = (typeof tm') (IS THIS NEEDED)
-  --constrain (Unify (TpData y itgs ips) (typeof tm')) >>
-  --freshTp >>= \ itp -> -- itp is the cases return type
-  --mapM (uncurry myInferCase) (pickyZip cs ctors) >>= \ cs'' -> -- infer the cases
-  -- add another constraint v that for each case `| x ps -> tm`, itp = (typeof tm)
-  --mapM_ (\ (Case x ps tm) -> constrain (Unify itp (typeof tm))) cs'' >>
   return (UsCase tm cs)
--- do nothing if we come across an UsTm that isn't an UsCase (or UsLet or UsLam eventually)
---desugar' x = CheckM x
 
-termToProg :: CheckM UsTm -> CheckM UsProgs
-termToProg (CheckM term)= UsProgs [] term
+termToProg :: UsTm -> UsProgs
+termToProg = UsProgs []
 
 -- Desugars an entire program
-desugarProgs :: UsProgs -> CheckM UsProgs
+desugarProgs :: UsProgs -> UsProgs
 desugarProgs (UsProgs xs term) = termToProg (desugar' term)
 
--- Try to desugar an entire file, running the CheckM monad
+-- Try to desugar an entire file
 desugarFile :: UsProgs -> Either String UsProgs
+desugarFile ps
+  | ps < 2 = Left "desugar error"
+  | otherwise = Right (desugarProgs ps)
+
+{-
 desugarFile ps =
   either (\ (e, loc) -> Left (if null (show loc) then show e else show e ++ ", " ++ show loc))
          (\ (a, s, w) -> Right a)
     (runExcept (runRWST (desugarProgs ps)
                         (CheckR emptyCtxt (Loc Nothing "")) mempty))
+-}
